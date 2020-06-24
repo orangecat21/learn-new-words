2d9717e957283ba3ac8283cbda2e4036b0eafe08
orangefruit21 <borovskih.05021999@gmail.com>
2020-06-25 00:00:20 +0300

    Nullable list item

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = () => <h1>Hello World!</h1>;
+const Header = () => <h1 className="App-header">Hello World!</h1>;
 
 const List = () => {
+  const listItems = ['Item 1', 'Item 2', 'Item 3'];
+  const authItem = <li>Item 0</li>;
+  const auth = true;
   return (
     <ul>
-      <li>First Item</li>
-      <li>Second Item</li>
+      {auth ? authItem : null}
+      {
+        listItems.map(item => <li>{ item }</li>)
+      }
     </ul>
   );
 }
