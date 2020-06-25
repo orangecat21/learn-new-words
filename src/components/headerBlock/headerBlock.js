1fa616bdbf5f580af591c5d0eeb87d2470b34054
orangefruit21 <borovskih.05021999@gmail.com>
2020-06-25 17:32:26 +0300

    New component

--- src/components/headerBlock/headerBlock.js
+++ src/components/headerBlock/headerBlock.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import s from './headerBlock.module.css';
+import logo from './logo.png';
+import {ReactComponent as LogoSVG} from '../../logo.svg';
 
-const Header = () => <h1 className={s.header}>Hello World!</h1>;
+const Header = () => {
+    return (
+        <div class={s.cover}>
+            <div class={s.wrap}>
+                <h1 class={s.header}>Учите слова онлайн</h1>
+                <img src={logo} alt='Logo'></img>
+                <LogoSVG/>
+                <p class={s.descr}>Воспользуйтесь карточками для запоминания и пополнения активныйх словарных запасов</p>
+            </div>
+        </div>
+    );
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -6,25 +6,24 @@ import './index.css';
 import Header from './components/headerBlock/headerBlock.js';
 
 
-const List = () => {
-  const listItems = ['Item 1', 'Item 2', 'Item 3'];
-  const authItem = <li>Item 0</li>;
-  const auth = false;
-  return (
-    <ul>
-      { auth ? authItem : null }
-      {
-        listItems.map((item, index) => <li key={ index }> { item } </li>)
-      }
-    </ul>
-  );
-}
+// const List = () => {
+//   const listItems = ['Item 1', 'Item 2', 'Item 3'];
+//   const authItem = <li>Item 0</li>;
+//   const auth = false;
+//   return (
+//     <ul>
+//       { auth ? authItem : null }
+//       {
+//         listItems.map((item, index) => <li key={ index }> { item } </li>)
+//       }
+//     </ul>
+//   );
+// }
 
 const App = () => {
   return (
     <>
       <Header/>
-      <List/>
     </>
   );
 }
