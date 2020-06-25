bdd9b3a51eb9eb5c606e6abb665c49a953a9262b
orangefruit21 <borovskih.05021999@gmail.com>
2020-06-25 15:48:58 +0300

    My first component on React.JS

---
+++ src/components/headerBlock/headerBlock.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import s from './headerBlock.module.css';
+
+const Header = () => <h1 className={s.header}>Hello World!</h1>;
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = () => <h1 className="App-header">Hello World!</h1>;
+import './index.css';
+
+import Header from './components/headerBlock/headerBlock.js';
+
 
 const List = () => {
   const listItems = ['Item 1', 'Item 2', 'Item 3'];
   const authItem = <li>Item 0</li>;
-  const auth = true;
+  const auth = false;
   return (
     <ul>
-      {auth ? authItem : null}
+      { auth ? authItem : null }
       {
-        listItems.map(item => <li>{ item }</li>)
+        listItems.map((item, index) => <li key={ index }> { item } </li>)
       }
     </ul>
   );
